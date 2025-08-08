74aab09172b088204b2d46b559950f097589e7ce
igorcastro1304 <igor.hoffmann@al.infnet.edu.br>
2025-08-07 20:06:37 -0400

    config: Fazendo as configurações necessárias para conexão com o BD

--- src/main/java/br/com/dio/board/Main.java
+++ src/main/java/br/com/dio/board/Main.java
@@ -1,13 +1,23 @@
 package br.com.dio.board;
 
+import br.com.dio.board.persistence.migration.MigrationStrategy;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @EnableFeignClients
 @SpringBootApplication
 public class Main {
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
+	}
+
+	@Bean
+	CommandLineRunner runMigrations(MigrationStrategy migrationStrategy) {
+		return args -> {
+			migrationStrategy.executeMigration();
+		};
 	}
 }
